b2b9e6121bfde24cab4113b440dcc5c19920e31f
YuLong Yao <feilongphone@gmail.com>
2021-07-19 05:41:47 +0800

    [g3kh]drop 430 reature

--- test/01_simple_c/main.c
+++ test/01_simple_c/main.c
@@ -1,3 +1,5 @@
-int main() {
-    return 0;
+void main() {
+    // int i = 0;
+
+    return;
 }
--- test/01_simple_c/test.fish
+++ test/01_simple_c/test.fish
@@ -2,7 +2,8 @@
 
 # ../../build/bin/clang -help
 # ../../build/bin/clang --print-multiarch
-../../build/bin/clang -print-targets
+# ../../build/bin/clang -print-targets
 # ../../build/bin/clang -print-supported-cpus 
 ../../build/bin/clang --target=g3kh main.c -emit-llvm -S -c -o ../../build/test/01_simple_c.ll
+../../build/bin/llc ../../build/test/01_simple_c.ll
 code ../../build/test/01_simple_c.ll
