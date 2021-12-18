82b030fd4954a4fbb5d6f79550e1d06d5c29963a
YuLong Yao <feilongphone@gmail.com>
2021-12-18 08:19:35 +0800

    [llvm][V800]add dummy MC stage.

--- llvm/lib/Target/V800/MCTargetDesc/V800MCTargetDesc.cpp
+++ llvm/lib/Target/V800/MCTargetDesc/V800MCTargetDesc.cpp
@@ -11,6 +11,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "V800MCTargetDesc.h"
+#include "V800MCAsmInfo.h"
 #include "TargetInfo/V800TargetInfo.h"
 #include "llvm/MC/MCInstrInfo.h"
 #include "llvm/MC/MCRegisterInfo.h"
@@ -19,4 +20,46 @@
 
 using namespace llvm;
 
-extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeV800TargetMC() {}
+static MCInstrInfo *createV800MCInstrInfo() {
+  MCInstrInfo *X = new MCInstrInfo();
+  return X;
+}
+
+static MCRegisterInfo *createV800MCRegisterInfo(const Triple &TT) {
+  MCRegisterInfo *X = new MCRegisterInfo();
+  return X;
+}
+
+static MCSubtargetInfo *
+createV800MCSubtargetInfo(const Triple &TT, StringRef CPU, StringRef FS) {
+  const llvm::MCSchedModel NoSchedModel = {};
+
+  const llvm::SubtargetSubTypeKV PD[] = {
+    { "temp_subtype", { { { 0x0ULL, 0x0ULL, 0x0ULL, 0x0ULL, } } }, { { { 0x0ULL, 0x0ULL, 0x0ULL, 0x0ULL, } } }, &NoSchedModel },
+  };
+
+  llvm::SubtargetFeatureKV PF[] = {
+    { "temp_feature", "feature placehold", 0x0ULL, { { { 0x0ULL, 0x0ULL, 0x0ULL, 0x0ULL, } } } },
+  };
+
+  const llvm::MCWriteProcResEntry WriteProcResTable[] = {{}};
+  const llvm::MCWriteLatencyEntry WriteLatencyTable[] = {{}};
+  const llvm::MCReadAdvanceEntry ReadAdvanceTable[] = {{}};
+
+  MCSubtargetInfo *X = new MCSubtargetInfo(TT, CPU, CPU, FS,
+                  PF, PD,
+                  WriteProcResTable, WriteLatencyTable,
+                  ReadAdvanceTable, nullptr,
+                  nullptr, nullptr );
+
+  return X;
+}
+
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeV800TargetMC() {
+  Target &T = getTheV800Target();
+
+  RegisterMCAsmInfo<V800MCAsmInfo> X(T);
+  TargetRegistry::RegisterMCInstrInfo(T, createV800MCInstrInfo);
+  TargetRegistry::RegisterMCRegInfo(T, createV800MCRegisterInfo);
+  TargetRegistry::RegisterMCSubtargetInfo(T, createV800MCSubtargetInfo);
+}
